d9f45a988573c3878810f2aacb0a2e5eb48d6574
Leonid Olivas <leonidolivas@gmail.com>
2020-08-09 14:00:23 +0300

    hw-05 done

---
+++ js/task-4.js
@@ -0,0 +1,36 @@
+class StringBuilder {
+  constructor(value) {
+    this._value = value;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  set value(value) {
+    this._value = value;
+  }
+
+  append(str) {
+    this._value += str;
+  }
+
+  prepend(str) {
+    this._value = str + this._value;
+  }
+
+  pad(str) {
+    this._value = str + this._value + str;
+  }
+}
+
+const builder = new StringBuilder(".");
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
